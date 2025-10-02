fbc507ff981f854da062f90acdfb081d56eb52ff
QAISER ALI <qaisers.official@gmail.com>
2025-10-02 13:34:07 +0500

    Feat: Add H1 component for styled headings because it is using twice in hero section

---
+++ src/components/common/H1.tsx
@@ -0,0 +1,19 @@
+interface H1Props {
+  children: React.ReactNode;
+  className?: string; 
+}
+
+const H1: React.FC<H1Props> = ({ children, className }) => {
+
+  const defaultClasses = 'text-4xl font-bold leading-10 text-[#000000]';
+
+  const combinedClasses = `${defaultClasses} ${className || ''}`.trim();
+
+  return (
+    <h1 className={combinedClasses}>
+      {children}
+    </h1>
+  );
+};
+
+export default H1;
