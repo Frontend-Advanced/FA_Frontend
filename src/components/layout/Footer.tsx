6701da3cdb01c367fe0eccc7218062bd4d39ccf5
QAISER ALI <qaisers.official@gmail.com>
2025-10-05 18:57:27 +0500

    Feat: Remove fixed height from Footer component for better responsiveness and gap

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -2,7 +2,7 @@ import { FOOTER_COLUMN_ONE, FOOTER_COLUMN_TWO } from "@/constants";
 
 const Footer = () => {
   return (
-    <footer className="w-full flex justify-center items-center h-16 py-[100px]">
+    <footer className="w-full flex justify-center items-center py-[100px]">
       <div className="max-w-6xl h-full flex flex-1 items-start justify-between md:flex-row flex-col gap-y-10">
         <h3>FA</h3>
         <div className="flex gap-10 md:flex-row flex-col">
