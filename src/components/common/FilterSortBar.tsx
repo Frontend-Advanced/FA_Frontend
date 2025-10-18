9006d47a8763451b80fd40832332de353f67b2fb
QAISER ALI <qaisers.official@gmail.com>
2025-10-18 15:13:58 +0500

    feat: update FilterSortBar layout and button styles for improved UI

--- src/components/common/FilterSortBar.tsx
+++ src/components/common/FilterSortBar.tsx
@@ -1,8 +1,16 @@
+import { ChevronDown } from "lucide-react"
+
 const FilterSortBar = () => {
   return (
-        <div className="border h-full w-[45%] md:w-[30%] flex items-center gap-1 md:gap-2.5 justify-end sm:flex-row flex-col">
-            <button className="border border-amber-300 w-full text-sm md:w-[25%] bg-black text-white rounded-full p-1 md:text-base md:px-3 md:py-1.5 transition hover:opacity-90 cursor-pointer">filter</button>
-            <button className="border border-amber-300 w-full text-sm md:w-[25%] bg-black text-white rounded-full p-1 md:text-base md:px-3 md:py-1.5 transition hover:opacity-90 cursor-pointer">sort</button>
+        <div className="w-[52%] md:w-[30%] flex items-end  gap-1 text-sm md:gap-2.5 justify-end sm:flex-row flex-col">
+            <button className="flex gap-0.5 items-center w-[74px] bg-black text-white rounded-full p-1.5 md:text-base md:px-3 transition hover:opacity-90 cursor-pointer hover:font-bold">
+                <ChevronDown size={20} strokeWidth={3.5} />
+                <span className=" text-sm">Filter</span>
+            </button>
+            <button className="flex gap-0.5 items-center w-[74px] bg-black text-white rounded-full p-1.5 md:text-base md:px-3 transition hover:opacity-90 cursor-pointer hover:font-bold">
+                <ChevronDown size={18} strokeWidth={3.5} />
+                <span className=" text-sm">Sort</span>
+            </button>
         </div>
   )
 }
